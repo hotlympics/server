0aaf6303eae9fa6584c2bb6d9838770805b73758
Jørgen Henriksen <joergen.henriksen@appear.net>
2025-08-09 14:09:35 +0200

    add tos

---
+++ src/config/tos-config.ts
@@ -0,0 +1,9 @@
+export const CURRENT_TOS_VERSION = '1.0';
+
+export const TOS_VERSIONS = {
+    '1.0': {
+        version: '1.0',
+        effectiveDate: '2024-01-01',
+        description: 'Initial Terms of Service',
+    },
+} as const;
